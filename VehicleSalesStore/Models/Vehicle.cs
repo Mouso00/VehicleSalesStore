61bdeee7a81b2c92f9654f9490326a552c10c1f8
Mouso00@DESKTOP <Mousahmed00@gmail.com>
2022-12-30 19:28:38 +0200

    2 menus 3 choices returning maker , year, price of the vehicles

---
+++ VehicleSalesStore/Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleSalesStore.Models
+{
+    class Vehicle
+    {
+
+        public string maker { get; set; }
+
+        public string year { get; set; }
+
+        public string price { get; set; }
+
+    }
+}
